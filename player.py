01b415914471fde62be873bf85c60062d534ed81
shrick <sickhickrick@women-at-work.org>
2023-09-09 22:14:47 +0200

    - command line parameter for window title - fast forward/rewind via row keys, steps in seconds via command line - use partial callbacks

--- player.py
+++ player.py
@@ -4,34 +4,51 @@ from functools import partial
 import tkinter as tk
 import vlc
 
+TITLE = "Shrick Video Player"
+FF_SECONDS = 10
+FR_SECONDS = 5
+
 def playback(player):
     player.set_pause(player.is_playing())
 
-def stop(player, root, delete=False):
+def forward(player, seconds):
+    player.set_time(player.get_time() + seconds * 1_000)
+
+def rewind(player, seconds):
+    player.set_time(player.get_time() - seconds * 1_000)
+
+def stop(player, root, filename, delete=False):
     player.stop()
     player.release()
     if delete:
-        print("TBD delete file")
+        print(f"TBD delete file '{filename}'")
     root.destroy()
 
-def get_filename():
+def get_commandline_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('filename')
+    parser.add_argument('-t', '--title', default=TITLE)
     parser.add_argument('-fs', '--full-screen', action='store_true')
+    parser.add_argument('-ff', '--fast-forward', type=int, default=FF_SECONDS)
+    parser.add_argument('-fr', '--fast-rewind', type=int, default=FR_SECONDS)
     args = parser.parse_args()
-    return args.filename, args.full_screen
+    return args.filename, args.title, args.full_screen, args.fast_forward, args.fast_rewind
 
 if __name__ == '__main__':
-    filename, fullscreen = get_filename()
+    # init
+    filename, title, fullscreen, ff, fr = get_commandline_arguments()
     player = vlc.MediaPlayer(filename)
-
     root = tk.Tk()
-    root.title("Shrick Video Player")
-    if fullscreen:
-        root.attributes('-fullscreen', True)
-    else:
-        root.geometry(f"{root.winfo_screenwidth()}x{root.winfo_screenheight()}+0+0")
 
+    # callbacks
+    playback_callback = partial(playback, player=player)
+    stop_callback = partial(stop, player=player, root=root, filename=filename)
+    delete_callback = partial(stop_callback, delete=True)
+    forward_callback = partial(forward, player=player, seconds=ff)
+    rewind_callback = partial(rewind, player=player, seconds=fr)
+
+    # controls
+    root.title(f"{title} ({filename})")
     tk.Grid.rowconfigure(root, 0, weight=1)
     tk.Grid.rowconfigure(root, 1, weight=0)
     tk.Grid.columnconfigure(root, 0, weight=1)
@@ -40,12 +57,13 @@ if __name__ == '__main__':
 
     frame = tk.Frame(root, bg='black')
     frame.grid(row=0, column=0, columnspan=3, sticky="NSEW")
+    player.set_xwindow(frame.winfo_id())
 
-    playback_callback = partial(playback, player=player)
-    stop_callback = partial(stop, player=player, root=root)
-    delete_callback = partial(stop_callback, delete=True)
+    if fullscreen:
+        root.attributes('-fullscreen', True)
+    else:
+        root.geometry(f"{root.winfo_screenwidth()}x{root.winfo_screenheight()}+0+0")
 
-    if not fullscreen:
         tk.Button(root, text="Play/Pause", fg="green",
             command=playback_callback).grid(row=1, column=0, sticky="SEW")
         tk.Button(root, text="Stop/Quit", fg="blue",
@@ -53,11 +71,14 @@ if __name__ == '__main__':
         tk.Button(root, text="Delete", fg="red",
             command=delete_callback).grid(row=1, column=2, sticky="SEW")
 
-    root.bind("<Escape>", lambda event: stop_callback())
+    # key bindings
+    root.bind('<Right>', lambda event: forward_callback())
+    root.bind('<Left>', lambda event: rewind_callback())
     root.bind("<space>", lambda event: playback_callback())
+    root.bind("<q>", lambda event: stop_callback())
+    root.bind("<Escape>", lambda event: stop_callback())
 
-    player.set_xwindow(frame.winfo_id())
+    # start
     player.play()
-
     root.focus_set()
     root.mainloop()
