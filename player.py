17249ca04786087d57daf8afc41a9deb495c38f3
shrick <sickhickrick@women-at-work.org>
2023-09-10 00:24:38 +0200

    Add context data class to manage after job and other variables
    - add context data class passed to stop/delete callback and progress update function
    - add dedicated function to delete file after confirming dialog
    - remove progress printing
    - check if file exists
    - abort if file cannot be played

--- player.py
+++ player.py
@@ -1,21 +1,31 @@
 # video player
 import argparse
+import pathlib
 from functools import partial
+from dataclasses import dataclass
 import tkinter as tk
 from tkinter import ttk
+from tkinter.messagebox import askyesno
 import vlc
 
 TITLE = "Shrick Video Player"
 FF_SECONDS = 10
 FR_SECONDS = 5
-PROGRESS_INTERVAL = 0.5
+PROGRESS_INTERVAL = int(0.5 * 1_000)
 
-def update_progress(player, root, progress):
-    percentage = player.get_position() * 100
+@dataclass(init=True)
+class PlayerContext:
+    root: ... = None
+    player: ... = None
+    filename: ... = None
+    progress: ... = None
+    job: ... = None
+
+def update_progress(context):
+    percentage = context.player.get_position() * 100
     if percentage < 99:
-        print(f"progress={percentage}%")
-        progress.set(percentage)
-        root.after(int(PROGRESS_INTERVAL * 1_000), lambda: update_progress(player, root, progress))
+        context.progress.set(percentage)
+        context.job = context.root.after(PROGRESS_INTERVAL, lambda: update_progress(context))
     else:
         stop(player, root)
 
@@ -30,12 +40,22 @@ def rewind(player, seconds):
     new_time = max(0, player.get_time() - seconds * 1_000)
     player.set_time(new_time)
 
-def stop(player, root, delete=None):
-    player.stop()
-    player.release()
-    if delete is not None:
-        print(f"TBD delete file '{delete}'")
-    root.destroy()
+def stop(context, delete=False):
+    if context.job is not None:
+        context.root.after_cancel(context.job)
+        context.job = None
+    context.player.stop()
+    context.player.release()
+    if delete:
+        delete_file(context.filename)
+    context.root.destroy()
+
+def delete_file(filename):
+    yes = askyesno(title="Confirmation", message=f"Really delete file '{filename}'?")
+    if yes:
+        print(f"Deleting file '{filename}'...")
+        file = pathlib.Path(filename)
+        file.unlink(missing_ok=True)
 
 def get_commandline_arguments():
     parser = argparse.ArgumentParser()
@@ -47,16 +67,20 @@ def get_commandline_arguments():
     args = parser.parse_args()
     return args.filename, args.title, args.full_screen, args.fast_forward, args.fast_rewind
 
-if __name__ == '__main__':
+def main():
     # init
     filename, title, fullscreen, ff, fr = get_commandline_arguments()
+    if not pathlib.Path(filename).is_file():
+        print(f"Error: File '{filename}' does not exist!")
+        exit(1)
     player = vlc.MediaPlayer(filename) # https://www.olivieraubert.net/vlc/python-ctypes/doc/vlc.MediaPlayer-class.html
     root = tk.Tk()
+    context = PlayerContext(root=root, player=player, filename=filename, progress=tk.IntVar(), job=None)
 
     # callbacks
     playback_callback = partial(playback, player=player)
-    stop_callback = partial(stop, player=player, root=root)
-    delete_callback = partial(stop_callback, delete=filename)
+    stop_callback = partial(stop, context=context)
+    delete_callback = partial(stop_callback, delete=True)
     forward_callback = partial(forward, player=player, seconds=ff)
     rewind_callback = partial(rewind, player=player, seconds=fr)
 
@@ -72,8 +96,7 @@ if __name__ == '__main__':
     frame.grid(row=0, column=0, columnspan=3, sticky="NSEW")
     player.set_xwindow(frame.winfo_id())
 
-    progress = tk.IntVar()
-    progressbar = ttk.Progressbar(variable=progress)
+    progressbar = ttk.Progressbar(variable=context.progress)
     progressbar.grid(row=1, column=0, columnspan=3, sticky="SEW")
 
     if fullscreen:
@@ -96,7 +119,15 @@ if __name__ == '__main__':
     root.bind("<Escape>", lambda event: stop_callback())
 
     # start
-    player.play()
-    root.after(0, lambda: update_progress(player, root, progress))
+    print(f"Now playing '{filename}'...")
+    started = player.play()
+    if started == -1:
+        print(f"Error: Could not play file '{filename}'!")
+        exit(1)
+
+    context.job = root.after(PROGRESS_INTERVAL, lambda: update_progress(context))
     root.focus_set()
     root.mainloop()
+
+if __name__ == '__main__':
+    main()
