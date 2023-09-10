4d7b29b5e55b1d0672e1e9feff981b6f71ee6ac3
shrick <sickhickrick@women-at-work.org>
2023-09-10 20:57:41 +0200

    Add bash script to play all files below given directory
    - pass thru of further command line arguments to player
    - evaluates player abort code for premature exit

---
+++ play.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+PLAYER_CMD="python player.py"
+PLAYER_TITLE="Zu Annas 18. Geburtstag"
+ABORT_CODE=23
+
+. .env/bin/activate
+
+VIDEO_DIR="$1"; shift
+find "$VIDEO_DIR" -type f | while read f; do
+    echo Playing "$f"...
+    $PLAYER_CMD "$f" -t "$PLAYER_TITLE" "$@"
+
+    if [ $? -eq $ABORT_CODE ]; then
+        echo Aborting...
+        break
+    fi
+done
