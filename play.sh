e23bc6dd3e7cfa68f3315f99d9f720c7922da0a3
shrick <sickhickrick@women-at-work.org>
2023-09-16 22:01:27 +0200

    Remove hard coded title

--- play.sh
+++ play.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 PLAYER_CMD="python player.py"
-PLAYER_TITLE="Zu Annas 18. Geburtstag"
 ABORT_CODE=23
 
 . .env/bin/activate
 
 VIDEO_DIR="$1"; shift
 find "$VIDEO_DIR" -type f | sort | while read f; do
-    $PLAYER_CMD "$f" -t "$PLAYER_TITLE" "$@"
+    $PLAYER_CMD "$f" "$@"
     if [ $? -eq $ABORT_CODE ]; then
         echo Aborting...
         break
